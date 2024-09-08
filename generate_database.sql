1d5cbe6564317cbbfbfcc2b2c4970ed4e0dc7c0a
Luiz Schonarth <luizcschonarth@gmail.com>
2024-09-08 17:44:44 -0300

    Adicionar trigger e procedure para winner_id de match

--- generate_database.sql
+++ generate_database.sql
@@ -1,17 +1,17 @@
 BEGIN;
 
-DROP TABLE IF EXISTS sponsorship;
-DROP TABLE IF EXISTS sponsor;
-DROP TABLE IF EXISTS coach;
-DROP TABLE IF EXISTS player;
-DROP TABLE IF EXISTS match;
-DROP TABLE IF EXISTS referee;
-DROP TABLE IF EXISTS location;
-DROP TABLE IF EXISTS ranking;
-DROP TABLE IF EXISTS team;
-DROP TABLE IF EXISTS tournament;
-DROP TABLE IF EXISTS modality;
-DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS sponsorship CASCADE;
+DROP TABLE IF EXISTS sponsor CASCADE;
+DROP TABLE IF EXISTS coach CASCADE;
+DROP TABLE IF EXISTS player CASCADE;
+DROP TABLE IF EXISTS match CASCADE;
+DROP TABLE IF EXISTS referee CASCADE;
+DROP TABLE IF EXISTS location CASCADE;
+DROP TABLE IF EXISTS ranking CASCADE;
+DROP TABLE IF EXISTS team CASCADE;
+DROP TABLE IF EXISTS tournament CASCADE;
+DROP TABLE IF EXISTS modality CASCADE;
+DROP TABLE IF EXISTS event CASCADE;
 
 CREATE TABLE event
 (
@@ -128,10 +128,36 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_winner_update
+CREATE OR REPLACE TRIGGER check_winner_update
 BEFORE INSERT OR UPDATE ON match
 FOR EACH ROW
 EXECUTE FUNCTION prevent_update_winner_before_match_end();
+
+CREATE OR REPLACE FUNCTION prevent_update_score_before_match()
+RETURNS trigger AS $$
+BEGIN
+    IF NEW.team1_score != OLD.team1_score OR NEW.team2_score != OLD.team2_score
+    AND CURRENT_TIMESTAMP < lower(NEW.duration) THEN
+        RAISE EXCEPTION 'Scores can only be updated during and after the match';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_score_update
+BEFORE INSERT OR UPDATE ON match
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_score_before_match();
+
+CREATE OR REPLACE PROCEDURE update_winners()
+LANGUAGE SQL
+BEGIN ATOMIC
+    UPDATE match SET winner_id = CASE
+        WHEN team1_score > team2_score THEN team1_id
+        WHEN team1_score < team2_score THEN team2_id
+        ELSE NULL
+    END WHERE CURRENT_TIMESTAMP AT TIME ZONE 'UTC' > upper(duration);
+END;
 
 -- TODO: generate data
 
