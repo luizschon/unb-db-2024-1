9e2c29e3255cad639fd5724e29f2cfd62508c490
Luiz Schonarth <luizcschonarth@gmail.com>
2024-09-08 15:30:49 -0300

    Remove NOT NULL do placar dos times na partida

--- generate_database.sql
+++ generate_database.sql
@@ -75,8 +75,8 @@ CREATE TABLE match
     tournament_id serial NOT NULL REFERENCES tournament (id) ON DELETE CASCADE,
     team1_id serial NOT NULL REFERENCES team (id) ON DELETE RESTRICT,
     team2_id serial NOT NULL REFERENCES team (id) ON DELETE RESTRICT,
-    team1_score integer NOT NULL DEFAULT 0,
-    team2_score integer NOT NULL DEFAULT 0,
+    team1_score integer DEFAULT 0,
+    team2_score integer DEFAULT 0,
     winner_id serial REFERENCES team (id) ON DELETE RESTRICT,
     location_id serial NOT NULL REFERENCES team (id) ON DELETE RESTRICT,
     referee_cpf character(11) NOT NULL REFERENCES referee (cpf) ON DELETE RESTRICT
