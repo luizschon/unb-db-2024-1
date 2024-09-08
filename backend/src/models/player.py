7139646c7a09d9c505e044d2a715d69314750d22
Luiz Schonarth <luizcschonarth@gmail.com>
2024-09-08 09:30:51 -0300

    Conserta nome da tabela player

--- backend/src/models/player.py
+++ backend/src/models/player.py
@@ -3,32 +3,32 @@ from typing_extensions import List
 from psycopg import sql
 from src.database import DatabaseConnection
 
-EVENT_SCHEMA = ('cpf', 'name', 'birthdate', 'starting', 'photo', 'team_id')
+PLAYER_SCHEMA = ('cpf', 'name', 'birthdate', 'starting', 'photo', 'team_id')
 
-class Event:
+class Player:
     @staticmethod
     def all() -> List[Dict]:
         with DatabaseConnection().cursor() as cur:
-            cur.execute("SELECT * from event")
+            cur.execute("SELECT * from player")
             rows = cur.fetchall()
         return rows
 
     @staticmethod
     def find_by_id(cpf) -> Dict | None:
         with DatabaseConnection().cursor() as cur:
-            cur.execute("SELECT * from event WHERE cpf = %s", (cpf,))
+            cur.execute("SELECT * from player WHERE cpf = %s", (cpf,))
             row = cur.fetchone()
         return row
 
     @staticmethod
     def create(**kwargs):
-        # Filtra valores recebidos que não pertencem ao schema da tabela Event
-        column_value_map = {k: v for k, v in kwargs.items() if k in EVENT_SCHEMA}
+        # Filtra valores recebidos que não pertencem ao schema da tabela Player
+        column_value_map = {k: v for k, v in kwargs.items() if k in PLAYER_SCHEMA}
         columns = column_value_map.keys()
         values = column_value_map.values()
 
         with DatabaseConnection().cursor() as cur:
-            query = sql.SQL("INSERT INTO event ({columns}) VALUES ({values})").format(
+            query = sql.SQL("INSERT INTO player ({columns}) VALUES ({values})").format(
                 columns=sql.SQL(',').join(list(map(lambda c: sql.Identifier(c), columns))),
                 values=sql.SQL(',').join(list(map(lambda v: sql.Literal(v), values)))
             )
@@ -37,13 +37,13 @@ class Event:
 
     @staticmethod
     def update(id, **kwargs):
-        # Filtra valores recebidos que não pertencem ao schema da tabela Event
-        column_value_map = {k: v for k, v in kwargs.items() if k in EVENT_SCHEMA}
+        # Filtra valores recebidos que não pertencem ao schema da tabela Player
+        column_value_map = {k: v for k, v in kwargs.items() if k in PLAYER_SCHEMA}
         columns = column_value_map.keys()
         values = column_value_map.values()
 
         with DatabaseConnection().cursor() as cur:
-            query = sql.SQL("UPDATE event SET ({columns}) = ({values})").format(
+            query = sql.SQL("UPDATE player SET ({columns}) = ({values})").format(
                 columns=sql.SQL(',').join(list(map(lambda c: sql.Identifier(c), columns))),
                 values=sql.SQL(',').join(list(map(lambda v: sql.Literal(v), values)))
             )
@@ -53,5 +53,5 @@ class Event:
     @staticmethod
     def delete(id):
         with DatabaseConnection().cursor() as cur:
-            cur.execute("DELETE FROM event WHERE cpf = %s", (cpf,))
+            cur.execute("DELETE FROM player WHERE cpf = %s", (cpf,))
         DatabaseConnection().commit()
